362ca7a8420df5ca28ef366fe9925dc08544331e
Luís Melo <luiseduardosousamelo@yahoo.com.br>
2024-11-14 10:46:40 -0300

    Implementando parte final/Tutorial 28:56

--- src/Pages/Signup/styles.js
+++ src/Pages/Signup/styles.js
@@ -22,3 +22,28 @@ export const Content = styled.div`
   padding: 20px;
   border-radius: 5px;
 `;
+
+export const Label = styled.label`
+  font-size: 18px;
+  font-weight: 600;
+  color: #676767;
+`;
+
+export const LabelSignin = styled.label`
+  font-size: 16px;
+  color: #676767;
+`;
+
+export const labelError = styled.label`
+  font-size: 14px;
+  color: red;
+`;
+
+export const Strong = styled.strong`
+  cursor: pointer;
+
+  a {
+    text-decoration: none;
+    color: #676767;
+  }
+`;
